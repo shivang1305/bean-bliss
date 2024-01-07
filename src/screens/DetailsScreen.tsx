873a2b98b096cbb21cb46b2a2a797d8e4b5fc453
Shivang Sharma <sharmashivang1999@gmail.com>
2024-01-07 22:07:14 +0530

    feat: Home screen ui is completed

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,7 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useStore} from 'zustand';
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation, route}: any) => {
+  const itemOfIndex = useStore((state: any) =>
+    route.params.type == 'Coffee' ? state.CoffeeList : state.BeanList,
+  )[route.params.index];
+
   return (
     <View>
       <Text>DetailsScreen</Text>
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -175,7 +175,14 @@ const HomeScreen = ({navigation}: any) => {
           keyExtractor={item => item.id}
           renderItem={({item}) => {
             return (
-              <TouchableOpacity onPress={() => navigation.push('Details')}>
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.push('Details', {
+                    index: item.index,
+                    id: item.id,
+                    type: item.type,
+                  })
+                }>
                 <CoffeeCard
                   id={item.id}
                   index={item.index}
@@ -207,7 +214,14 @@ const HomeScreen = ({navigation}: any) => {
           keyExtractor={item => item.id}
           renderItem={({item}) => {
             return (
-              <TouchableOpacity onPress={() => navigation.push('Details')}>
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.push('Details', {
+                    index: item.index,
+                    id: item.id,
+                    type: item.type,
+                  })
+                }>
                 <CoffeeCard
                   id={item.id}
                   index={item.index}
