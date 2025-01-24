534367ea482bfb34f6768492103314d6c4f21c10
Shivang Sharma <sharmashivang1999@gmail.com>
2025-01-24 20:14:55 +0530

    fix: typo

--- src/screens/details/DetailsScreen.tsx
+++ src/screens/details/DetailsScreen.tsx
@@ -1,12 +1,7 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {useStore} from 'zustand';
 
-const DetailsScreen = ({navigation, route}: any) => {
-  const itemOfIndex = useStore((state: any) =>
-    route.params.type === 'Coffee' ? state.CoffeeList : state.BeanList,
-  )[route.params.index];
-
+const DetailsScreen = () => {
   return (
     <View>
       <Text>DetailsScreen</Text>
